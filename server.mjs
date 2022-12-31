8319521692bed83fab2b277260f97407ac59ca7e
Ronak Mehta <rmehta2510@gmail.com>
2022-12-31 14:08:52 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 import { createServer } from 'http';
 
 createServer((req, res) => {
-  res.write('Hello World!');
+  res.write('Hello World from India!');
   res.end();
 }).listen(process.env.PORT);
